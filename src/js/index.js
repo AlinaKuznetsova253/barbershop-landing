0aa06d0d1ea2021c4a7f0bcef74593453475a8e6
Alina <kuznetsova646@gmail.com>
2025-07-18 01:47:02 +0500

    blocks added header and hero

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,35 @@
 import "./swiper";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+
+  const menuBtn = document.querySelector(".menu__btn-mobile");
+  const menuMobile = document.querySelector(".menu-mobile");
+  const menuLinks = document.querySelectorAll("[data-link='mobile-menu']");
+
+  const toggleMobileMenu = () => {
+    if (!isMenuOpen) {
+      menuMobile.classList.remove("menu-closing");
+      menuMobile.classList.add("menu-open");
+      menuBtn.classList.add("menu-open");
+      document.body.style.overflow = "hidden";
+      isMenuOpen = true;
+    } else {
+      menuMobile.classList.add("menu-closing");
+      menuBtn.classList.remove("menu-open");
+      document.body.style.overflow = "inherit";
+
+      setTimeout(() => {
+        menuMobile.classList.remove("menu-closing");
+        menuMobile.classList.remove("menu-open");
+        isMenuOpen = false;
+      }, 500);
+    }
+  };
+  let isMenuOpen = false;
+
+  menuBtn.addEventListener("click", toggleMobileMenu);
+
+  menuLinks.forEach((link) => link.addEventListener("click", toggleMobileMenu));
+});
